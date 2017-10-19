14a8e2e58a3fb7ce13c663f99d339f1464804a47
Limbo <m.mazraeshahi@gmail.com>
2017-10-19 23:29:43 +0330

    Update Book entity fields

--- src/AppBundle/Entity/Book.php
+++ src/AppBundle/Entity/Book.php
@@ -23,10 +23,25 @@ class Book
      */
     private $name;
     /**
-     * @ORM\Column(type="string",length=50)
+     * @ORM\Column(type="string",length=50,nullable=true)
      */
     private $title;
-
+    /**
+     * @ORM\Column(type="string",length=50)
+     */
+    private $author;
+    /**
+     * @ORM\Column(type="string",length=50)
+     */
+    private $publisher;
+    /**
+     * @ORM\Column(type="date",nullable=true)
+     */
+    private $publishDate;
+    /**
+     * @ORM\Column(type="string",length=50,nullable=true)
+     */
+    private $isbn;
 
     /**
      * Get id
@@ -84,5 +99,101 @@ class Book
     public function getTitle()
     {
         return $this->title;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     *
+     * @return Book
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set publisher
+     *
+     * @param string $publisher
+     *
+     * @return Book
+     */
+    public function setPublisher($publisher)
+    {
+        $this->publisher = $publisher;
+
+        return $this;
+    }
+
+    /**
+     * Get publisher
+     *
+     * @return string
+     */
+    public function getPublisher()
+    {
+        return $this->publisher;
+    }
+
+    /**
+     * Set publishDate
+     *
+     * @param \DateTime $publishDate
+     *
+     * @return Book
+     */
+    public function setPublishDate($publishDate)
+    {
+        $this->publishDate = $publishDate;
+
+        return $this;
+    }
+
+    /**
+     * Get publishDate
+     *
+     * @return \DateTime
+     */
+    public function getPublishDate()
+    {
+        return $this->publishDate;
+    }
+
+    /**
+     * Set isbn
+     *
+     * @param string $isbn
+     *
+     * @return Book
+     */
+    public function setIsbn($isbn)
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    /**
+     * Get isbn
+     *
+     * @return string
+     */
+    public function getIsbn()
+    {
+        return $this->isbn;
     }
 }
